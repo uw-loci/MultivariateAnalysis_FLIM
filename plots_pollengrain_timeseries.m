bf76cd801f5a6138ff13aa2b697f562bec37f8ca
jenuc <jenuvchacko@gmail.com>
2022-10-11 10:55:58 -0500

    adding some plot to see outputdata2

--- plots_pollengrain_timeseries.m
+++ plots_pollengrain_timeseries.m
@@ -4,15 +4,20 @@
 outputdata_cell = table2cell(outputdata2);
 
 m=['+' , 'o' , '*' , '.' , 'x' , 'square' , 'diamond' , 'v' ,'^' , '>' , '<' , 'pentagram' , 'hexagram' ] ;
-
-for pollen_index = 1:npollen
+c = []
     figure()
+    hold on;
+for pollen_index = 1:npollen
+
     for file_index = 1:nfiles
 
     %mean_ = outputdata2(1,1).pollen_1{1}.mean ; 
-    y_val = outputdata_cell{file_index,pollen_index}.mean ;
+    %y_val = outputdata_cell{file_index,pollen_index}.mean ;
+    %scatter( file_index, y_val );%,'r','marker', m(pollen_index) );
+    y_val = outputdata_cell{file_index,pollen_index}.std / (outputdata_cell{file_index,pollen_index}.mean *outputdata_cell{file_index,pollen_index}.area^2 );
+    x_val = outputdata_cell{file_index,pollen_index}.mean ;
 
-    scatter( file_index, y_val );%,'r','marker', m(pollen_index) );
+    scatter( x_val, y_val );%,'r','marker', m(pollen_index) );
 
     end
 end
